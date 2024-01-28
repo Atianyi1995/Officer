caf145c4375d4814dbbe4ba16b62deb7fb88b549
Atianyi1995 <38279283+Atianyi1995@users.noreply.github.com>
2024-01-28 03:27:23 +0300

    Andrew - Update

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f, movespeed = 20;
+
+    public GameObject Slipper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+        Slipper.transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
